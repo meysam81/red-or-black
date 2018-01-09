e5f3906ad4d93c1138b5910f8e0fd355a721c600
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-09 20:25:13 +0330

    not complete, but a template

--- main.cpp
+++ main.cpp
@@ -8,43 +8,43 @@ int main(int argc, char *argv[])
 
 
     do
-        {
-            system("clear"); // to clear the terminal for linux; change the argument to 'cls' for windows
-            cout << setw(30) << "RED BLACK TREE\n";
-            cout << "  1. Insert in the tree\n"
-                 << "  2. Delete a node from the tree\n"
-                 << "  3. Search for an element in the tree\n"
-                 << "  4. Display the tree\n"
-                 << "  5. Exit\n\n"
-                 << "Enter your choice: ";
-            int input;
-            cin >> input;
-            switch (input) {
-            case 1:
+    {
+        system("clear"); // to clear the terminal for linux; change the argument to 'cls' for windows
+        cout << setw(30) << "RED BLACK TREE\n";
+        cout << "  1. Insert in the tree\n"
+             << "  2. Delete a node from the tree\n"
+             << "  3. Search for an element in the tree\n"
+             << "  4. Display the tree\n"
+             << "  5. Exit\n\n"
+             << "Enter your choice: ";
+        int input;
+        cin >> input;
+        switch (input) {
+        case 1:
 
-                break;
-            case 2:
+            break;
+        case 2:
 
-                break;
-            case 3:
+            break;
+        case 3:
 
-                break;
-            case 4:
+            break;
+        case 4:
 
-                break;
-            case 5:
-                exit(0);
-                break;
-            default:
-                break;
-            }
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            break;
+        }
 
-            // to hold the screen for showing the results
-            cout << "\nPress any key to continue...\n" << flush;
-            cin.ignore();
-            cin.ignore();
+        // to hold the screen for showing the results
+        cout << "\nPress any key to continue...\n" << flush;
+        cin.ignore();
+        cin.ignore();
 
-        } while (true);
+    } while (true);
 
     return 0;
 }
