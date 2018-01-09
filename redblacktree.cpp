6a748c5f5ac9ac19f245f9b81df43b61828e16cb
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-09 20:26:41 +0330

    bst add, still working on red black thing

--- redblacktree.cpp
+++ redblacktree.cpp
@@ -1,6 +1,75 @@
 #include "redblacktree.h"
 
-RedBlackTree::RedBlackTree()
+bool RedBlackTree::insert(int x)
 {
+    node *p = root;
+    node *q = new node(x);
 
+    if (p == nullptr)
+    {
+        p = q;
+        p->color = node::BLACK;
+        return true;
+    }
+
+    while (true)
+    {
+        if (p->value < x) // one way to access a pointer's member
+        {
+            if (p->right != nullptr)
+                p = p->right;
+            else
+            {
+                p->right = q;
+                q->parent = p;
+                break;
+            }
+
+        }
+        else if ((*p).value > x) // another way to access a pointer's member
+        {
+            if (p->left != nullptr)
+                p = p->left;
+            else
+            {
+                p->left = q;
+                q->parent = p;
+                break;
+            }
+        }
+        else
+        {
+            // node already exists
+            return false;
+        }
+
+    }
+
+
+    // still need some work
+    if (q->parent->color != node::BLACK)
+    {
+        node *uncle;
+        if (p->parent->right != p)
+            uncle = p->parent->right;
+        else
+            uncle = p->parent->left;
+
+        if (uncle->color == node::RED)
+        {
+            p->color = node::BLACK;
+            uncle->color = node::BLACK;
+            p->parent->color = uncle->parent->color = // it's the same, no difference
+                    node::RED;
+        }
+        else // if (uncle->color == node::BLACK)
+        {
+
+        }
+    }
+}
+
+RedBlackTree::RedBlackTree() : root(nullptr)
+{
+//     = node::BLACK;
 }
--- redblacktree.h
+++ redblacktree.h
@@ -1,10 +1,26 @@
+
+#include "node.h"
+
 #ifndef REDBLACKTREE_H
 #define REDBLACKTREE_H
 
 
+/* Forward declaration for circular relationship
+ * because we wanted to add RedBlackTree as a friend to node
+ * and also add a field of type node in the class RedBlackTree
+ * as it is a circular relationship, we need a what so called 'forward declaration'
+ * */
+class node;
+
+
 class RedBlackTree
 {
+private:
+
+
+    node *root;
 public:
+    bool insert(int x);
     RedBlackTree();
 };
 
